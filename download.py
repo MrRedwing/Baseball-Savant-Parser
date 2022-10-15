d3503ab0038abe44e0e13e00ad76729123f86e8a
MrRedwing <45808840+MrRedwing@users.noreply.github.com>
2022-10-14 22:42:17 -0500

    Update download.py
    Removed concatenate clip. It was unused and required another library

--- download.py
+++ download.py
@@ -63,20 +63,6 @@ def download_video(path, link, number):
 
     return
 
-def concatenate_clips(path, title):
-    mypath = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
-    final_path = os.path.join(mypath, title+'.mp4')
-
-    clip_paths = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f)) and 'clip_' in f]
-    clip_paths.sort(key=lambda x: int(x.replace('clip_','').replace('.mp4','')))
-
-    text_file = os.path.join(mypath, 'mylist.txt')
-    with open(text_file, 'w') as f:
-        for path in clip_paths:
-            f.write('file \''+path+'\'\n')
-
-    os.system(f'ffmpeg -y -f concat -safe 0 -i {text_file} -c copy {final_path}')
-
 def validate(text, comp1='y', comp2='n'):
     answer = input(text).strip().lower()
 
